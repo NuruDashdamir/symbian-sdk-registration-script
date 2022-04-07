e13e067fe46c67edbd5ef3e144e31d53b9861b37
Nuru TaşDemir <49725539+NuruTasDemir@users.noreply.github.com>
2022-04-07 15:08:37 +0400

    Update Reset_S60_SDK_Trial.cmd

--- Reset_S60_SDK_Trial.cmd
+++ Reset_S60_SDK_Trial.cmd
@@ -3,11 +3,12 @@ color 2F
 REM Script creator is "NuruTasDemir", feel free to use, share and modify :)
 REM Thanks to "nikita36078" for telling me "Nokia SDKs Registration Info" path in regedit
 REM Thanks to "bent" who is creator of EKA2L1 Symbian Emulator
-REM If you interested in with EKA2L1 Symbian emulator visit https://discord.gg/5Bm5SJ9
+REM If you are interested with EKA2L1 Symbian emulator, visit https://discord.gg/5Bm5SJ9
 
-REM For working "wmic", "regini" and "reg" commands required.
-REM Tested on Windows 7 x64 and dont require admin rights.
-REM Worked for S60v3FP2, S60v5 and S^3 SDKs. It will work for other SDKs probably. 
+REM "wmic", "regini" and "reg" commands required for the script to work
+REM Tested on Windows XP, 7, 8.1 and 10 (both x86 and x64) and dosen't require admin rights.
+REM Worked for S60v3, S60v3FP1, S60v3FP2, S60v5 and S^3 SDKs.
+
 REM this for loop gets User Security ID and assign it to userSID variable
 for /f "delims= " %%a in ('"wmic path win32_useraccount where name='%UserName%' get sid"') do (
    if not "%%a"=="SID" (          
@@ -24,7 +25,6 @@ del temporary.txt
 
 REM this reg command deletes registry folder to reset SDK trial 
 reg delete "HKEY_CURRENT_USER\Software\Nokia\Product Registration" /f
-echo All registration info deleted already.
 echo Your S60 Emulator have 14 days trial again.
 echo.
 pause
