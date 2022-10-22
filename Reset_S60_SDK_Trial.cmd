90df8429498b625974e7f3f74149f3d3abf8c2cf
Nuru Dashdamirli <49725539+NuruDashdamir@users.noreply.github.com>
2022-10-22 15:06:51 +0400

    comment update

--- Reset_S60_SDK_Trial.cmd
+++ Reset_S60_SDK_Trial.cmd
@@ -1,12 +1,13 @@
 @echo off
 color 2F
-REM Script creator is "NuruTasDemir", feel free to use, share and modify :)
+REM Script creator is "NuruDashdamir", feel free to use, share and modify :)
 REM Thanks to "nikita36078" for telling me "Nokia SDKs Registration Info" path in regedit
 REM Thanks to "bent" who is creator of EKA2L1 Symbian Emulator
 REM If you are interested with EKA2L1 Symbian emulator, visit https://discord.gg/5Bm5SJ9
 
 REM "wmic", "regini" and "reg" commands required for the script to work
 REM Tested on Windows XP, 7, 8.1 and 10 (both x86 and x64) and dosen't require admin rights.
+REM Won't work in Windows 11 because "wmic" tool is removed in Windows 11.
 REM Worked for S60v3, S60v3FP1, S60v3FP2, S60v5 and S^3 SDKs.
 
 REM this for loop gets User Security ID and assign it to userSID variable
